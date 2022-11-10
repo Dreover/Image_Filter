522f92fe932e5b9c96e7615e03822aa83c76c8e3
Dondre Overton <dondrebanksoverton@Dondres-Air.lan>
2022-11-10 14:15:29 -0500

    listbox shown

--- app.py
+++ app.py
@@ -14,11 +14,18 @@ frame.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(N, S, E, W))
 #buttons
 browse_btn = ttk.Button(content, text="Browse")
 apply_btn = ttk.Button(content, text="Apply")
-fil_list = Listbox(content, height=5)
+
+
+#populate listbox
+choices = [1, 2, 3, 4, 5]
+choice_var = StringVar(value=choices)
+fil_list = Listbox(content, listvariable=choice_var, height=5)
 
 #layout config
 browse_btn.grid(column=3, row=3, padx=10)
 apply_btn.grid(column=4, row=3)
+fil_list.grid(column=3, row=1, padx=30)
+fil_list.config(height=15, width=25)
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 content.columnconfigure(0, weight=3)
