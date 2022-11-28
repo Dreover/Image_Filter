1e50e705c65f13d576fd071473e454d849709660
Dreover <dreover2@gmail.com>
2022-11-28 17:47:37 -0500

    .

--- app.py
+++ app.py
@@ -3,25 +3,28 @@ from tkinter import ttk
 from tkinter import filedialog
 from PIL import ImageTk, Image
 
-def browse():
+img = None
+def upload():
+    global img
+    f_types = [("PNG Files", "*.png"), ("All Files", "*.*"),
+               ("Jpg Files", "*.jpg")]
     img_path = filedialog.askopenfilename(title="Select an Image",
-                                          filetypes=[("PNG Files", "*.png"),                                                    
-                                                     ("All Files", "*.*")])
+                                          filetypes=f_types)    
     img = ImageTk.PhotoImage(Image.open(img_path))
-    img_label = ttk.Label(frame, image=img)
+    place_img = ttk.Button(frame, image=img)
 root = Tk()
 root.title("Apply a filter to an image!")
 
 #Image Containter
 content = ttk.Frame(root, padding=(3,3,12,12))
-frame = ttk.Frame(content, borderwidth=5, relief="ridge", width=550, height=350)
+frame = ttk.Frame(content, width=550, height=350)
 content.grid(column=0, row=0, sticky=(N, S, E, W))
 frame.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(N, S, E, W))
 
 #Buttons
 apply_btn = ttk.Button(content, text="Apply")
 save_btn = ttk.Button(content, text="Save")
-browse_btn = ttk.Button(content, text="Browse", command=browse)
+upload_btn = ttk.Button(content, text="Upload", command=upload)
 
 #Labels
 fil_label = ttk.Label(content, text="Select a filter below")
@@ -39,8 +42,8 @@ save_btn.config(width=10)
 fil_list.grid(column=3, row=1, padx=30)
 fil_list.config(height=5, width=10)
 fil_label.grid(column=3, row=0)
-browse_btn.grid(column=4, row=0)
-browse_btn.config(width=10)
+upload_btn.grid(column=4, row=0)
+upload_btn.config(width=10)
 
 #Grid Config
 root.columnconfigure(0, weight=1)
